6969f934c535d83357c95518f74531382e0f9924
schmartmann <stefanhartmann@gmail.com>
2019-01-10 16:44:06 -0500

    add new workout component

---
+++ src/components/workout_builder.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class WorkoutBuilder extends Component {
+  render() {
+    return (
+      <div className="workout-builder">
+      </div>
+    )
+  }
+}
+
+export default WorkoutBuilder;
